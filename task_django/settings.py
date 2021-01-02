bb2f0ad767f66c64e8228768afe0af76f9eda29f
mvmolloy <mvamolloy@gmail.com>
2021-01-02 13:40:38 +0000

    replacing base_dir path

--- task_django/settings.py
+++ task_django/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
